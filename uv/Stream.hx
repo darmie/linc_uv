f0013bf77d4d444c325c663b3e03c0c0806d248f
Kevin Leung <kevinresol@gmail.com>
2018-11-29 12:08:36 +0800

    experimenting....

--- uv/Stream.hx
+++ uv/Stream.hx
@@ -1,4 +1,84 @@
 package uv;
 
 import uv.Uv;
+import uv.EventEmitter;
 import cpp.*;
+import haxe.io.Bytes;
+
+
+@:access(uv)
+@:unreflective
+class Stream<Handle:StreamHandle> extends Finalizable {
+	public var onConnect(default, null):EventEmitter1<Stream<Handle>> = new EventEmitter1();
+	public var onRead(default, null):EventEmitter1<Bytes> = new EventEmitter1();
+	public var onEnd(default, null):EventEmitter0 = new EventEmitter0();
+	
+	var loop:Loop;
+	var handle:Handle;
+	
+	public function new(loop:Loop, handle:Handle) {
+		super();
+		this.loop = loop;
+		this.handle = handle;
+		handle.data = untyped __cpp__('this');
+	}
+	
+	public inline function listen(backlog:Int) {
+		return Uv.listen(cast handle, backlog, Callable.fromStaticFunction(_onConnect));
+	}
+	
+	public inline function accept(client:Stream<Handle>) {
+		return Uv.accept(cast handle, cast client.handle);
+	}
+	
+	public inline function readStart() {
+		return Uv.read_start(cast handle, Callable.fromStaticFunction(_onAlloc), Callable.fromStaticFunction(_onRead));
+	}
+	
+	override function finalize() {
+		Stdlib.free((cast handle:Pointer<Stream_t>));
+		loop = null;
+		handle = null;
+	}
+	
+	static function _onConnect<Handle:StreamHandle>(stream:Star<Stream_t>, status:Int):Void {
+		var stream:Stream<Handle> = untyped __cpp__('(hx::Object*){0}->data', stream);
+		var client = cast new Tcp(stream.loop); // TODO: should be new Stream?
+		stream.accept(client); // TODO: handle error
+		stream.onConnect.emit(client);
+	}
+	
+	static function _onAlloc<Handle:StreamHandle>(stream:Star<Handle_t>, suggestedSize:Size_t, buf:Star<Buf_t>):Void {
+		var stream:Stream<Handle> = untyped __cpp__('(hx::Object*){0}->data', stream);
+		var suggestedSize:Int = cast suggestedSize;
+		var buf:BufHandle = cast buf;
+		var arr = NativeArray.create(suggestedSize);
+		var base:Pointer<Char> = NativeArray.address(arr, 0);
+		buf.base = cast base;
+		buf.len = suggestedSize;
+	}
+	
+	static function _onRead<Handle:StreamHandle>(stream:Star<Stream_t>, nread:Long, buf:ConstStar<Buf_t>):Void {
+		var stream:Stream<Handle> = untyped __cpp__('(hx::Object*){0}->data', stream);
+		var nread:Int = cast nread;
+		var buf:BufHandle = cast buf;
+		
+		if(nread > 0) {
+			var bytes = Bytes.alloc(nread);
+			Stdlib.memcpy(NativeArray.address(bytes.getData(), 0), cast buf.base, nread);
+			stream.onRead.emit(bytes);
+		}
+		
+		if(nread < 0) {
+			if(nread != Uv.EOF) {
+				trace('error: $nread');
+				// TODO: emit error
+			}
+			
+			var handle:Pointer<Stream_t> = cast stream.handle;
+			var handle:Pointer<Handle_t> = handle.reinterpret();
+			Uv.close(cast handle.ref, null);
+			stream.onEnd.emit();
+		}
+	}
+}
--- uv/Uv.hx
+++ uv/Uv.hx
@@ -16,6 +16,12 @@ extern class Uv {
 	@:native("UV_RUN_DEFAULT")
 	public static var RUN_DEFAULT:RunMode;
 	
+	// error
+	@:native("uv_strerror")
+	public static function strerror(code:Int):ConstCharStar;
+	@:native("uv_err_name")
+	public static function err_name(code:Int):ConstCharStar;
+	
 	// loop
 	@:native("uv_default_loop")
 	public static function default_loop():LoopHandle;
@@ -23,8 +29,14 @@ extern class Uv {
 	public static function loop_init(loop:LoopHandle):Int;
 	@:native("uv_run")
 	public static function run(loop:LoopHandle, mode:RunMode):Int;
-	@:native("uv_loop_close")
-	public static function loop_close(loop:LoopHandle):Int;
+	
+	// timer
+	@:native("uv_timer_init")
+	public static function timer_init(loop:LoopHandle, timer:TimerHandle):Int;
+	@:native("uv_timer_start")
+	public static function timer_start(timer:TimerHandle, cb:TimerCallback, timeout:UInt64_t, repeat:UInt64_t):Int;
+	@:native("uv_timer_stop")
+	public static function timer_stop(timer:TimerHandle):Int;
 	
 	// tcp
 	@:native("uv_tcp_init")
@@ -39,12 +51,16 @@ extern class Uv {
 	public static function accept(stream:StreamHandle, client:StreamHandle):Int;
 	@:native("uv_read_start")
 	public static function read_start(stream:StreamHandle, alloc_cb:AllocCallback, read_cb:ReadCallback):Int;
+	@:native("uv_read_stop")
+	public static function read_stop(stream:StreamHandle):Int;
 	@:native("uv_close")
 	public static function close(stream:Handle, cb:CloseCallback):Int;
 	
 	// misc
 	@:native("uv_ip4_addr")
 	public static function ip4_addr(ip:String, port:Int, addr:SockAddrInHandle):Int;
+	@:native("uv_hrtime")
+	public static function hrtime():UInt64_t;
 }
 
 
@@ -81,6 +97,14 @@ extern class TcpHandle extends StreamHandle {}
 extern class Tcp_t {}
 
 @:include('linc_uv.h')
+@:native('cpp::Reference<uv_timer_t>')
+extern class TimerHandle extends Handle {}
+
+@:include('linc_uv.h')
+@:native('uv_timer_t')
+extern class Timer_t {}
+
+@:include('linc_uv.h')
 @:native('cpp::Reference<uv_stream_t>')
 extern class StreamHandle extends Handle {}
 
@@ -108,12 +132,20 @@ extern class SockAddrHandle {}
 extern class SockAddrInHandle {}
 
 @:include('linc_uv.h')
+@:native('sockaddr_in')
+extern class SockAddrIn {}
+
+@:include('linc_uv.h')
 @:native('size_t')
 extern class Size_t {}
 
 @:include('linc_uv.h')
 @:native('ssize_t')
 extern class SSize_t {}
+
+@:include('linc_uv.h')
+@:native('uint64_t')
+extern class UInt64_t {}
 
 @:include('linc_uv.h')
 @:native('long')
@@ -123,3 +155,4 @@ typedef ConnectionCallback = Callable<Star<Stream_t>->Int->Void>;
 typedef AllocCallback = Callable<Star<Handle_t>->Size_t->Star<Buf_t>->Void>;
 typedef ReadCallback = Callable<Star<Stream_t>->Long->ConstStar<Buf_t>->Void>;
 typedef CloseCallback = Callable<Star<Handle_t>->Void>;
+typedef TimerCallback = Callable<Star<Timer_t>->Void>;
